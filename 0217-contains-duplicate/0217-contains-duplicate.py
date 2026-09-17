9546a0a3d7e2939372c632fc6c838a255feb64a7
Mahek <mahekyadav30@gmail.com>
2026-09-17 18:03:28 +0530

    Time: 9 ms (85.81%), Space: 32.1 MB (91.1%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        return len(nums) != len(set(nums))
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
