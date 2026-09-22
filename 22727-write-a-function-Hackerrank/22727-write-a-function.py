d107cdf91712612d4230905166fc49eef7374617
Mahek <mahekyadav30@gmail.com>
2026-09-22 19:09:51 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 22727-write-a-function-Hackerrank/22727-write-a-function.py
@@ -0,0 +1,18 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
