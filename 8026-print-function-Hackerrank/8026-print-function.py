ebe45c8cf33ea24b161d5f55ae6e6c07ea9c3aac
Mahek <mahekyadav30@gmail.com>
2026-09-23 15:47:06 +0530

    Force committed solution - Time: O(n), Space: O(1)

---
+++ 8026-print-function-Hackerrank/8026-print-function.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    i = 1
+    while i <= n:
+        print(i, end='')
+        
+        i += 1
+        
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
